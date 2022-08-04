1398e099950f806650e684edc5e0c3cf2922ddcb
narmnathan <narmnathan@gmail.com>
2022-08-04 00:40:47 -0400

    added ability to read song metadata + play from file

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 # to-do: needs to be able to read files, read metadata from files, organize albums/tracks, and play songs
+# to-do: add a way for user to specify filepath of folder before execution
 
+from tinytag import TinyTag
 from pathlib import Path
+from playsound import playsound
+
+filepath = '/Users/narmnathan/Documents/Music/PNDCOLOURS/02 - Girl From Oakland.mp3'
+
+tag = TinyTag.get(filepath)
+
+album = tag.album
+artist = tag.artist
+title = tag.title
+
+def playSong():
+    print(('Now Playing: %s -- %s, %s') % (title, artist, album))
+    playsound(filepath)
+
+playSong()
 

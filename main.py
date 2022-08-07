53b1228b3603925e96d5dccdb091b924ef32d0e3
narmnathan <narmnathan@gmail.com>
2022-08-06 22:05:07 -0400

    added ability to locate file directory, import music to playlist, play from playlist

--- main.py
+++ main.py
@@ -1,24 +1,66 @@
-# to-do: needs to be able to read files, read metadata from files, organize albums/tracks, and play songs
-# to-do: add a way for user to specify filepath of folder before execution
+import os, glob, tinytag, playsound
+# to-do: introduce play/pause/skip functionality, save user_playlist to file ?
 
-from tinytag import TinyTag
-from pathlib import Path
-from playsound import playsound
+class User:
+    default_path = os.path.expanduser('~/Music')
+    default_listdir = os.listdir(default_path)
+    print(default_listdir)  # show all folders in Music directory
 
-filepath = '/Users/narmnathan/Documents/Music/PNDCOLOURS/02 - Girl From Oakland.mp3'
+    def __init__(self):
+        self.path = os.path.expanduser('~/Music') # default user Music path
+        self.playlist = {} # import music to here
 
-tag = TinyTag.get(filepath)
+    def import_file(self): # import folders to sort into dictionary as per import_dict():
+        while True:
+            file_input = input('enter the name of the folder or file to import: \n')
+            file_path = '%s/%s' % (self.path, file_input)
+            if os.path.isdir(file_path):
+                os.chdir(file_path)
+                self.path = os.getcwd()
+                print(self.path)
+                print(os.listdir(self.path))
+                user_choice = input('enter \'import\' to import: \n')
+                if user_choice == 'import':
+                    User.import_dict(self)
+                break
+            else:
+                print(file_path + ' does not exist.')
+                continue
 
-album = tag.album
-artist = tag.artist
-title = tag.title
+    def import_dict(self): # have Python take the file names in a folder, sort them, and assign them keys that are path
+        file_path = self.path
+        print(file_path)
+        glob_list = file_path + '/*.mp3'
+        print(glob_list)
+        import_list = glob.glob(glob_list)
+        import_list.sort()
+        print(import_list)
+        for index in import_list:
+            print(index)
+            tag = tinytag.TinyTag.get(index)
+            self.playlist[tag.title] = index
+        user_choice = input('enter \'playlist\' to see playlist: \n')
+        if user_choice == 'playlist':
+            self.user_playlist()
 
-def play():
-    print(('Now Playing: %s -- %s, %s') % (title, artist, album))
-    playsound(filepath)
+    def user_playlist(self): # display user playlist
+        for number, key in enumerate(self.playlist, start=1):
+            print(str(number) + ' - ' + str(key))
+        self.play_song()
 
-while True:
-    print('enter \'play\' to play, \'pause\' to pause, or \'skip\' to skip:')
-    choice = input()
-    if choice == 'play':
-        play()
+    def play_song(self): # play songs from playlist
+        user_select = input('enter the name of a song to play: \n')
+        while True:
+            tag = tinytag.TinyTag.get(self.playlist[str(user_select)])
+            album = tag.album
+            artist = tag.artist
+            title = tag.title
+
+            print('now playing: %s - %s, %s' % (title, artist, album))
+            playsound.playsound(self.playlist[str(user_select)])
+
+
+user_import = User()
+user_import.import_file()
+
+
--- test.py
+++ test.py
@@ -1,25 +1,27 @@
-import os, glob
-path = os.path.expanduser('~/Music')
-listdir = os.listdir(path)
-print(listdir) # show all folders in Music directory
-#playlist = {}
+import os, glob, tinytag, playsound
+# to-do: introduce play/pause/skip functionality, save user_playlist to file ?
 
-class UserImport:
-    def __init__(self, path):
-        self.path = os.path.expanduser('~/Music')
+class User:
+    default_path = os.path.expanduser('~/Music')
+    default_listdir = os.listdir(default_path)
+    print(default_listdir)  # show all folders in Music directory
+
+    def __init__(self):
+        self.path = os.path.expanduser('~/Music') # default user Music path
+        self.playlist = {} # import music to here
 
     def import_file(self): # import folders to sort into dictionary as per import_dict():
         while True:
-            file_input = input('enter the name of the folder or file to import:')
-            file_path = '%s/%s' % (path, file_input)
+            file_input = input('enter the name of the folder or file to import: \n')
+            file_path = '%s/%s' % (self.path, file_input)
             if os.path.isdir(file_path):
-                os.chdir(file_path) # change this to import folder/file into a dictionary that stores the song name as key and path as value
+                os.chdir(file_path)
                 self.path = os.getcwd()
                 print(self.path)
                 print(os.listdir(self.path))
-                user_choice = input('enter \'import\' to import:')
+                user_choice = input('enter \'import\' to import: \n')
                 if user_choice == 'import':
-                    UserImport.import_dict(self)
+                    User.import_dict(self)
                 break
             else:
                 print(file_path + ' does not exist.')
@@ -27,18 +29,37 @@ class UserImport:
 
     def import_dict(self): # have Python take the file names in a folder, sort them, and assign them keys that are path
         file_path = self.path
-        # print('now importing...')
         print(file_path)
-        #print(os.listdir(file_path))
-        import_playlist = list(os.listdir(file_path))
-        sorted(import_playlist, reverse=True)
-        print(import_playlist)
+        glob_list = file_path + '/*.mp3'
+        print(glob_list)
+        import_list = glob.glob(glob_list)
+        import_list.sort()
+        print(import_list)
+        for index in import_list:
+            print(index)
+            tag = tinytag.TinyTag.get(index)
+            self.playlist[tag.title] = index
+        user_choice = input('enter \'playlist\' to see playlist: \n')
+        if user_choice == 'playlist':
+            self.user_playlist()
+
+    def user_playlist(self): # display user playlist
+        for number, key in enumerate(self.playlist, start=1):
+            print(str(number) + ' - ' + str(key))
+        self.play_song()
+
+    def play_song(self): # play songs from playlist
+        user_select = input('enter the name of a song to play: \n')
+        while True:
+            tag = tinytag.TinyTag.get(self.playlist[str(user_select)])
+            album = tag.album
+            artist = tag.artist
+            title = tag.title
+
+            print('now playing: %s - %s, %s' % (title, artist, album))
+            playsound.playsound(self.playlist[str(user_select)])
 
 
-
-
-
-
-
-user_import = UserImport(path)
+user_import = User()
 user_import.import_file()
+

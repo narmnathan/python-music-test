c3c5575475fcdef42b5fab9a643d2367175a548b
narmnathan <narmnathan@gmail.com>
2022-08-08 20:36:15 -0400

    filesystem module

---
+++ files.py
@@ -0,0 +1,73 @@
+import os, glob, tinytag
+# establish directory to read and import music files from
+# allow user to set directory and choose files to import
+# imported files are stored and returned in user's Songs list
+
+
+class fileSystem:
+    def __init__(self):
+        self.dir = os.path.expanduser('~/Music')
+        self.songs = {}
+
+    def listdir(self):
+        # prints mp3 files in current directory
+        # update to include .wav
+        directory = glob.glob(self.dir + '/*.mp3')
+        os.listdir(self.dir)
+        if not directory:
+            print('no mp3 files found! \n')
+        else:
+            print(directory)
+        user_choice = input('[1] to set directory, [2] to import files, [3] to exit \n')
+        if user_choice == '1':
+            self.chdir()
+        elif user_choice == '2':
+            if not directory:
+                print('no mp3 files to import... \n')
+                self.listdir()
+            else:
+                self.file_import()
+        else:
+            print('exiting...')
+
+    def chdir(self):
+        # prints directory and allows user to set path
+        print('current directory: ' + self.dir)
+        print(os.listdir(self.dir))
+        user_choice = input('enter directory to read: ')
+        # change later
+        while True:
+            if os.path.isdir(user_choice) is True:
+                self.dir = user_choice
+                os.chdir(self.dir)
+                self.listdir()
+                break
+            elif os.path.isdir('%s/%s' % (self.dir, user_choice)) is True:
+                self.dir = '%s/%s' % (self.dir, user_choice)
+                os.chdir(self.dir)
+                self.listdir()
+                break
+            else:
+                print('invalid input. \n')
+                self.chdir()
+                break
+
+    def file_import(self):
+        i = 1
+        for index in glob.glob(self.dir + '/*.mp3'):
+            tag = tinytag.TinyTag.get(index)
+            print('[%s] - %s' % (i, tag.title))
+            i += 1
+        user_choice = input('enter [0] to import all listed files: \n')
+        # update to have user choose range of songs to import
+        if user_choice == '0':
+            for index in glob.glob(self.dir + '/*.mp3'):
+                tag = tinytag.TinyTag.get(index)
+                self.songs[tag.title] = index
+            return self.songs
+        print('success!')
+        # ideally this will import all files to user's Songs list
+
+
+file = fileSystem()
+file.listdir()
